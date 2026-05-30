b5402d48e514ea73c1ca25b126ab0c2a55363a24
Giacomo <scanavini.giacomo@gmail.com>
2026-05-30 19:34:12 +0800

    (NEW) included calculated fields

---
+++ 04_calculated_fields.sql
@@ -0,0 +1,54 @@
+/* CALCULATED FIELDS */
+
+/* concatenating fields for strings 
+    literally joins the content of the rows in to a new column */
+SELECT CONCAT(firstName, ' ', lastName) AS fullName
+SELECT CONCAT(firstName, '-', lastName) AS fullName
+FROM Employees
+
+/* string manipulation */
+SELECT UPPER(firstName) AS upperName
+SELECT LOWER(firstName) AS lowerName
+FROM Employees
+
+/* remove unwanted spaces with TRIM */
+SELECT LTRIM(firstName) AS cleanFirstName -- remove spaces on the left
+SELECT RTRIM(firstName) AS cleanFirstName -- remove spaces on the right
+SELECT TRIM(firstName) AS cleanFirstName  -- remove any space from right or left
+FROM Employees
+
+
+/* mathematical calculations */
+SELECT voltage, resistance, voltage / resistance AS current
+FROM tableName
+
+/* conditional clauses with CASE WHEN */
+SELECT 
+    customer_name,
+    total_spend,
+    CASE
+        WHEN total_spend >= 1000 THEN 'high'
+        WHEN total_spend >= 500 THEN 'medium'
+        ELSE 'low'
+    END AS customer_type
+FROM Customers;
+
+/* default values with COALESCE 
+    COALESCE is particularly useful to handle missing values (NULL) 
+    since mathematical operations with NULL return a NULL value */
+SELECT 
+    product_name
+    COALESCE(discount, 0) AS discount_value
+FROM Products;
+
+SELECT 
+    price
+    COALESCE(discount, 0) AS discount,
+    print - COALESCE(discount, 0) AS final_price
+FROM Products;
+
+SELECT 
+    firstName, 
+    lastName,
+    COALESCE(phone_mobile, phone_home, phone_work, 'No phone') -- The first non-NULL expression is returned
+FROM Employees;
